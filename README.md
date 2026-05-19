47d607bbbf5bcbddf3651f648a7b12bae0fbf85d
ktrn-s <rynasuh@gmail.com>
2026-05-19 03:42:34 -0400

    Initial weather app commit

---
+++ README.md
@@ -0,0 +1,53 @@
+Weather Forecast Web Application
+
+Description
+
+It helps users quickly check the current weather and see a 5-day forecast, so you can plan your day or week accordingly without opening multiple sites or apps.
+
+The application provides clear and user-friendly input for:
+
+- City name
+
+After entering a city and pressing Enter, the application instantly displays:
+
+- Current temperature
+- “Feels like” temperature
+- Weather conditions (e.g., Sunny, Rainy, Snowy)
+- Minimum and maximum temperatures for the day
+- 5-day forecast with daily highs, lows, and weather conditions
+
+Optional enhancements include dynamic backgrounds and icons that change according to the weather, providing a more engaging visual experience.
+
+Live Demo
+
+###
+
+Screenshot
+
+![](./##.png)
+
+Technologies Used
+
+- JavaScript
+- HTML
+- CSS
+- OpenWeatherMap API (for weather data)
+
+How to Run the Project
+
+No additional setup is required.
+
+1. Clone the repository:
+ bash 
+git clone https://###
+
+2. Open the project folder and open `index.html` in your browser.
+
+Challenges and Learnings
+
+One of the main challenges in this project was working with API data and asynchronous JavaScript to fetch and display weather information correctly.
+
+Implementing the 5-day forecast required understanding how to parse JSON data and dynamically update the DOM.
+This experience strengthened my understanding of real-world API usage, JavaScript event handling, and user interface design.
+
+Future improvements could include hourly forecasts, autocomplete city search, and more advanced visual effects like animated weather icons.
---
+++ script.js
@@ -0,0 +1,175 @@
+const api = {
+    //link
+    endpoint:"https://api.openweathermap.org/data/2.5/",
+    //API key
+    key: "e73a76eecedc0954f83b82e5d43455f9"
+}
+
+const input = document.querySelector("#input");
+input.addEventListener("keypress", enter)
+
+function enter(e) {
+    if (e.key === "Enter") {
+        getInfo(input.value);
+    }
+}
+
+async function getInfo (data) {
+    const res = await fetch(
+        `${api.endpoint}forecast?q=${data}&units=metric&appid=${api.key}`
+    );
+    const result = await res.json();
+    console.log(result);
+    displayResult(result);
+    displayForecast(result);
+}
+
+function displayResult(result) {
+    //showing 
+    document.querySelector("#weather-card").style.display = "block";
+    document.querySelector("#forecast").style.display = "flex";
+    document.querySelector("#welcome").style.display = "none";
+    //time
+    let time = document.querySelector("#time");
+    time.textContent = getCityTime(result.city.timezone);
+
+    // // first forecast 
+    let weather = result.list[0];
+    const icon = getWeatherIcon(weather.weather[0].main);
+    setBackground(weather.weather[0].main);
+    // city
+    let city = document.querySelector("#city");
+    city.textContent = `${result.city.name}, ${result.city.country}`;
+    //date
+    getOurDate();
+    //temperature
+    let temperature = document.querySelector("#temperature");
+    temperature.innerHTML = `${Math.round(weather.main.temp)}<span>°</span>`;
+    //feels like
+    let feelsLike = document.querySelector("#feelsLike");
+    feelsLike.innerHTML = `${Math.round(weather.main.feels_like)}<span>°</span>`;
+    //conditions
+    let conditions = document.querySelector("#conditions");
+    conditions.textContent =`${icon} ${weather.weather[0].main}`;
+    //min max
+    let variations = document.querySelector("#variations");
+    variations.innerHTML = 
+        `Min: ${Math.round(weather.main.temp_min)}<span>°</span>
+        Max:${Math.round(weather.main.temp_max)}<span>°</span>` ;
+}
+//showing 3 days forecast
+function displayForecast(result) {
+    const forecast = document.querySelector("#forecast");
+    forecast.innerHTML = "";
+
+    // every 8 items = 24 hours
+    for(let i = 0; i < result.list.length; i += 8) {
+        const item = result.list[i];
+        // date
+        const date = new Date(item.dt_txt);
+        // day names
+        const days = [
+            "Sun", "Mon", "Tue",
+            "Wed", "Thu", "Fri", "Sat"
+        ];
+        const day = days[date.getDay()];
+        const icon = getWeatherIcon(item.weather[0].main);
+        forecast.innerHTML += `
+        <div class="card">
+            <h3>${day}</h3>
+            <p style="font-size: 22px">${icon}</p>
+            <p>
+            ${Math.round(item.main.temp)}°
+            </p>
+            <p>
+            ${item.weather[0].main}
+            </p>
+        </div>
+        `;
+    }
+}
+
+
+function getOurDate(){
+    const myDate = new Date();
+    const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+    let day = days[myDate.getDay()];
+    console.log(day);
+
+    let todayDate = myDate.getDate();
+    console.log(todayDate);
+
+    let month = months[myDate.getMonth()];
+    console.log(month);
+
+    let year = myDate.getFullYear();
+    console.log(year);
+
+    let showDate = document.querySelector("#date");
+    showDate.innerHTML =
+    `${day} ${todayDate} ${month} ${year}`;
+    // showDate.innerHTML = `${day}`+` `+`${todayDate}`+` `+`${month}`+` `+`${year}`;
+}
+
+function setBackground(condition) {
+
+    const body = document.body;
+
+    if (condition === "Clear") {
+        body.style.background =
+        "linear-gradient(180deg, #4facfe, #00f2fe)";
+    }
+
+    else if (condition === "Rain") {
+        body.style.background =
+        "linear-gradient(180deg, #2c3e50, #4ca1af)";
+    }
+
+    else if (condition === "Snow") {
+        body.style.background =
+        "linear-gradient(180deg, #e6dada, #274046)";
+    }
+
+    else {
+        body.style.background =
+        "linear-gradient(180deg, #1e293b, #0f172a)";
+    }
+}
+
+function getWeatherIcon(condition) {
+
+    if (condition === "Clear") return "☀️";
+    if (condition === "Rain") return "🌧";
+    if (condition === "Snow") return "❄️";
+    if (condition === "Clouds") return "☁️";
+
+    return "🌡️";
+}
+
+//time
+// function getTime() {
+//     const now = new Date();
+
+//     let hours = now.getHours();
+//     let minutes = now.getMinutes();
+
+//     if (minutes < 10) minutes = "0" + minutes;
+
+//     return `${hours}:${minutes}`;
+// }
+
+function getCityTime(timezoneOffsetSeconds) {
+
+    const utc = new Date().getTime() + new Date().getTimezoneOffset() * 60000;
+
+    const cityTime = new Date(utc + timezoneOffsetSeconds * 1000);
+
+    let hours = cityTime.getHours();
+    let minutes = cityTime.getMinutes();
+
+    if (minutes < 10) minutes = "0" + minutes;
+
+    return `${hours}:${minutes}`;
+}
